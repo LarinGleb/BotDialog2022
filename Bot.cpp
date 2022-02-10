b48b760bc9a4608fa1f1841d2ee4a16d029f930d
VforVindetta <gleblarinpo@gmail.com>
2022-02-10 16:10:17 +0300

    Added sql requests and test buttons

--- Bot.cpp
+++ Bot.cpp
@@ -1,36 +1,7 @@
 #include "Bot.h"
 
 
-std::vector<std::string> ArgumentsCommand(std::string command) {
-    size_t indexStart = 1;
-    size_t indexEnd; 
-    
-    std::string argument;
-    std::vector<std::string> arguments;
-
-    while ((indexEnd = command.find(SEPARATOR, indexStart)) != std::string::npos) {
-        argument = command.substr(indexStart, indexEnd - indexStart);
-        indexStart = indexEnd + 1;
-        arguments.push_back(argument);
-    }
-
-    arguments.push_back(command.substr(indexStart));
-    arguments.erase(arguments.begin());
-    
-    return arguments;
-}
-
-
-std::string ConcatString(std::vector<std::string> concat) {
-    std::string concatString;
-    for (std::string word: concat) {
-        concatString += word + " ";
-    }
-    return concatString;
-}
-
-
-int InitBotCommands(TgBot::Bot &bot) {
+int InitBotCommands(TgBot::Bot& bot) {
     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
         bot.getApi().sendMessage(message->chat->id, "Hi!");
     });
@@ -40,10 +11,11 @@ int InitBotCommands(TgBot::Bot &bot) {
 }
 
 
-int GetHash(std::string String) {
+size_t GetHash(std::string String) {
     std::hash<std::string> hashThisString;
-    return int(hashThisString(String));
+    return hashThisString(String);
 }
+
 
 int main() {
     TgBot::Bot bot("1940339152:AAG0jf4jVUk62ZmSHyPmrjlYCoMYPR-qjNk");
@@ -54,16 +26,16 @@ int main() {
     db_api::Connector conn(hostname.c_str(), username.c_str(), password.c_str(), "Reviews");
 
     InitBotCommands(bot);
+    InitReviewBot(bot);
+    
+
     bot.getEvents().onAnyMessage([&bot, &conn](TgBot::Message::Ptr message) {
         if (StringTools::startsWith(message->text, "/start")) {
             return;
         }
-        else if(StringTools::startsWith(message->text, "/create")) {
-            std::string nameEvent = ConcatString(ArgumentsCommand(message->text));
-            conn.AddEvent(nameEvent, GetHash(nameEvent));
-            return;
-        }
     });
+
+
     TgBot::TgLongPoll longPoll(bot);
     while (true) {
         std::cout << "Long poll started" << "\n";
--- Bot.h
+++ Bot.h
@@ -5,22 +5,22 @@
 #include <tgbot/tgbot.h> // bot th lib
 #include <iostream> // standart lib
 #include "Connector/DataBase.h" // for working with sql
-#include <string>  // for working string
-#include <vector> // for concat 
 #include <functional>  // for hashing
+#include "BotTools/BotTools.h"
+#include "ReviewBot/ReviewBot.h"
+
 
 // Errors and Succes keys
 #define ErrorCommandInit 2
 #define ErrorCommandWork 3
 #define Success 0
 
+// Get hash by string 
+size_t GetHash(std::string String); 
 
-#define SEPARATOR " "
+// Standart init commands for Bot
+int InitBotCommands(TgBot::Bot& bot); 
 
-int GetHash(std::string String); // Get hash by string 
-std::string ConcatString(std::vector<std::string> concat); // Return Concat string of massive
-std::vector<std::string> ArgumentsCommand(const std::string command); // Return massive of arguments
-int InitBotCommands(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message); // Standart init commands for Bot
 int main(); // main function 
 
 #endif
