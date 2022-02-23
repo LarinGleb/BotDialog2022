e229b42366eb8eaaac003c252dbdbe87b4392ab5
VforVindetta <gleblarinpo@gmail.com>
2022-02-23 11:39:45 +0300

    Button and arguments

--- BotTools/Button.cpp
+++ BotTools/Button.cpp
@@ -2,10 +2,9 @@
 
 
 TgBot::InlineKeyboardButton::Ptr CreateButtonTG(const std::string textButton, const std::string callBackData) {
-    TgBot::InlineKeyboardButton::Ptr WriteNew(new TgBot::InlineKeyboardButton);
-    WriteNew->text = textButton;
-    WriteNew->callbackData = callBackData;
-    return WriteNew;
+    TgBot::InlineKeyboardButton::Ptr Button(new TgBot::InlineKeyboardButton);
+    Button->text = textButton;
+    Button->callbackData = callBackData;
+    return Button;
+
 }
-
-
