999274042882d73901f7ac60c9ea4561026300d9
VforVindetta <gleblarinpo@gmail.com>
2022-02-23 11:40:51 +0300

    Review Bot library

--- ReviewBot/ReviewBot.h
+++ ReviewBot/ReviewBot.h
@@ -6,7 +6,19 @@
 #include <iostream>
 #include "../BotTools/BotTools.h"
 #include <vector>
+#include <cmath>
 
-int InitReviewBot(TgBot::Bot& bot);
+#define COUNT_EVENT_IN_PAGE 10
+#define Success 0
+
+int InitReviewBot(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
+int DeleteEventState(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
+std::string GetPageMessage(int page, std::vector<std::string> events);
+int ListEventsState(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
+int StartCreatingState(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
+int MenuCreateEventCP(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
+int ChooseTypeEvent(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
+int MenuCreateCP(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
+int MenuDeleteCP(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message, std::vector<std::string> namesCP);
 
 #endif
