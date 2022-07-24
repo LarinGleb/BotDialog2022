75ead75883c5447a170aa391f58060d1dfc407fb
VforVindetta <gleblarinpo@gmail.com>
2022-07-24 21:29:11 +0300

    Logic for review bot

--- ReviewBot/ReviewBot.cpp
+++ ReviewBot/ReviewBot.cpp
@@ -1,177 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <string>
+
+#include <tgbot/tgbot.h>
+
 #include "ReviewBot.h"
+#include "../BotTools/BotTools.h"
 
-int InitReviewBot(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message) {
+namespace review_bot {
+    int InitBot(TgBot::Bot& bot, std::int64_t chatId, bool admin) {
+        TgBot::InlineKeyboardMarkup::Ptr keyboardInit(new TgBot::InlineKeyboardMarkup);
+        keyboardInit->inlineKeyboard.push_back(CreateLineButtons({{"Оставить отзыв", ADD_REVIEW}}));
 
-    TgBot::InlineKeyboardMarkup::Ptr keyboardInit(new TgBot::InlineKeyboardMarkup);
-    std::vector<TgBot::InlineKeyboardButton::Ptr> firstLineButtons;
-    std::vector<TgBot::InlineKeyboardButton::Ptr> secondLineButtons;
-    std::vector<TgBot::InlineKeyboardButton::Ptr> thirdLineButtons;
-
-    firstLineButtons.push_back(CreateButtonTG("Добавить мероприятие", "AddEvent"));
-    firstLineButtons.push_back(CreateButtonTG("Удалить мероприятие", "DeleteEvent"));
-
-    secondLineButtons.push_back(CreateButtonTG("Add Review", "AddReview"));
-    secondLineButtons.push_back(CreateButtonTG("Delete Review", "DeleteReview"));
-
-    thirdLineButtons.push_back(CreateButtonTG("Список мероприятий", "List"));
-    
-    keyboardInit->inlineKeyboard.push_back(firstLineButtons);
-    keyboardInit->inlineKeyboard.push_back(secondLineButtons);
-    keyboardInit->inlineKeyboard.push_back(thirdLineButtons);
-
-    bot.getApi().sendMessage(message->chat->id, "Hello", false, 0, keyboardInit);
-
-    return Success;
-
-}
-
-int ChooseTypeEvent(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message) {
-
-    TgBot::InlineKeyboardMarkup::Ptr keyboardChooseType(new TgBot::InlineKeyboardMarkup);
-    
-    std::vector<TgBot::InlineKeyboardButton::Ptr> firstLineButtons;
-    std::vector<TgBot::InlineKeyboardButton::Ptr> secondLineButtons;
-
-    firstLineButtons.push_back(CreateButtonTG("КП'шное", "CreateEventWithCP"));
-    firstLineButtons.push_back(CreateButtonTG("Не КП'шное", "CreateEventWithOUTCP"));
-
-    secondLineButtons.push_back(CreateButtonTG("Назад (в меню создания мероприятия)", "ReturnToMenuCreateEvent"));
-
-    keyboardChooseType->inlineKeyboard.push_back(firstLineButtons);
-    keyboardChooseType->inlineKeyboard.push_back(secondLineButtons);
-
-    bot.getApi().sendMessage(message->chat->id, "Выберите тип мероприятия: ", false, 0, keyboardChooseType);
-}   
-
-
-int StartCreatingState(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message) {
-
-    TgBot::InlineKeyboardMarkup::Ptr keyboardCreate(new TgBot::InlineKeyboardMarkup);
-    std::vector<TgBot::InlineKeyboardButton::Ptr> firstLineButtons;
-    std::vector<TgBot::InlineKeyboardButton::Ptr> secondLineButtons;
-    std::vector<TgBot::InlineKeyboardButton::Ptr> thirdLineButtons;
-    
-    firstLineButtons.push_back(CreateButtonTG("Выбрать имя", "Name")); 
-    firstLineButtons.push_back(CreateButtonTG("Добавить информацию об мероприятии", "Information")); 
-    firstLineButtons.push_back(CreateButtonTG("Указать дату проведения", "Date")); 
-
-    secondLineButtons.push_back(CreateButtonTG("Выбрать тип мероприятия", "ChooseType"));
-
-    thirdLineButtons.push_back(CreateButtonTG("Добавить команду", "CreateCommand"));
-    thirdLineButtons.push_back(CreateButtonTG("Удалить команду", "DeleteCommand"));
-
-    keyboardCreate->inlineKeyboard.push_back(firstLineButtons);
-    keyboardCreate->inlineKeyboard.push_back(secondLineButtons);
-    keyboardCreate->inlineKeyboard.push_back(thirdLineButtons);
-    
-
-    bot.getApi().sendMessage(message->chat->id, "Вы в меню создания мероприятия. Здесь вы можете настроить всё необходимое. По стандарту мероприятие не КП'шное. ", false, 0, keyboardCreate);
-
-    return Success;
-}
-
-
-int MenuCreateCP(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message) {
-    TgBot::InlineKeyboardMarkup::Ptr keyboardMenuCP(new TgBot::InlineKeyboardMarkup);
-    std::vector<TgBot::InlineKeyboardButton::Ptr> firstLineButtons;
-    
-    firstLineButtons.push_back(CreateButtonTG("Ввести имя КП", "AddNameCP")); // 
-    firstLineButtons.push_back(CreateButtonTG("Ввести информацию об КП", "AddNameInfoCP")); //
-    
-    std::vector<TgBot::InlineKeyboardButton::Ptr> secondLineButtons;
-
-    secondLineButtons.push_back(CreateButtonTG("Ввести имя препода на КП", "AddNameTeacherCP")); //
-    secondLineButtons.push_back(CreateButtonTG("Удалить препода из КП", "AddDeleteTeacherCP")); //
-
-    std::vector<TgBot::InlineKeyboardButton::Ptr> thirdLineButtons;
-
-    thirdLineButtons.push_back(CreateButtonTG("Добавить данное КП", "AddNewCP")); // 
-    thirdLineButtons.push_back(CreateButtonTG("Очистить информацию об КП", "ClearCP")); //
-
-    std::vector<TgBot::InlineKeyboardButton::Ptr> fourthLineButtons;
-
-    fourthLineButtons.push_back(CreateButtonTG("Назад (в меню создания КП'шного мероприятия)", "ReturnToMenuCreateEventСP"));
-
-    keyboardMenuCP->inlineKeyboard.push_back(firstLineButtons);
-    keyboardMenuCP->inlineKeyboard.push_back(secondLineButtons);
-    keyboardMenuCP->inlineKeyboard.push_back(thirdLineButtons);
-    keyboardMenuCP->inlineKeyboard.push_back(fourthLineButtons);
-    
-    bot.getApi().sendMessage(message->chat->id, "Вы в меню создания КП: ", false, 0, keyboardMenuCP);
-
-    return Success;
-}
-//✓
-
-
-int MenuDeleteCP(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message, std::vector<std::string> namesCP) {
-    TgBot::InlineKeyboardMarkup::Ptr keyboardDeleteCP(new TgBot::InlineKeyboardMarkup);
-    for (int i = 0; i < namesCP.size(); i ++) {
-        std::vector<TgBot::InlineKeyboardButton::Ptr> LineButtons;
-        LineButtons.push_back(CreateButtonTG(namesCP[i], "CP" + std::to_string(i)));
-        keyboardDeleteCP->inlineKeyboard.push_back(LineButtons);
-    }
-    bot.getApi().sendMessage(message->chat->id, "Выберите КП для удаления: ", false, 0, keyboardDeleteCP);
-}
-
-
-int MenuCreateEventCP(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message) {
-
-    TgBot::InlineKeyboardMarkup::Ptr keyboardMenuCreateEvent(new TgBot::InlineKeyboardMarkup);
-    std::vector<TgBot::InlineKeyboardButton::Ptr> firstLineButtons;
-
-    firstLineButtons.push_back(CreateButtonTG("Добавить КП", "AddCP")); //
-    firstLineButtons.push_back(CreateButtonTG("Удалить КП", "DeleteCP")); // 
-
-    std::vector<TgBot::InlineKeyboardButton::Ptr> secondLineButtons;
-
-    secondLineButtons.push_back(CreateButtonTG("Очистить всё", "Clear"));
-
-    keyboardMenuCreateEvent->inlineKeyboard.push_back(secondLineButtons);
-    keyboardMenuCreateEvent->inlineKeyboard.push_back(firstLineButtons);
-
-    bot.getApi().sendMessage(message->chat->id, "Вы в меню создания КП'шного мероприятия: ", false, 0, keyboardMenuCreateEvent);
-
-    return Success;
-}
-
-
-int DeleteEventState(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message) {
-
-    TgBot::InlineKeyboardMarkup::Ptr keyboardDelete(new TgBot::InlineKeyboardMarkup);
-    std::vector<TgBot::InlineKeyboardButton::Ptr> firstLineButtons;
-    firstLineButtons.push_back(CreateButtonTG("List", "List"));
-    keyboardDelete->inlineKeyboard.push_back(firstLineButtons);
-
-    bot.getApi().sendMessage(message->chat->id, "Введите название мероприятия: ", false, 0, keyboardDelete);
-
-    return Success;
-
-}
-
-
-std::string GetPageMessage(int page, std::vector<std::string> events) {
-    int countEvents = events.size();
-    std::string eventMessage = "";
-    for (int i = 0; i < countEvents; i++) {
-        if (i >= page * COUNT_EVENT_IN_PAGE && i < (page + 1) * COUNT_EVENT_IN_PAGE) {
-            eventMessage += events[i] + '\n';
+        if (admin) {
+            keyboardInit->inlineKeyboard.push_back(CreateLineButtons({{"Добавить мероприятие", ADD_EVENT}}));
+            keyboardInit->inlineKeyboard.push_back(CreateLineButtons({{"Читать отзывы", READ_REVIEW}}));
         }
-    }
-    return eventMessage;
-}
+        bot.getApi().sendMessage(chatId, "Привяу!", false, 0, keyboardInit);
 
-
-int ListEventsState(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message) {
-    TgBot::InlineKeyboardMarkup::Ptr keyBoardList(new TgBot::InlineKeyboardMarkup);
-    std::vector<TgBot::InlineKeyboardButton::Ptr> firstLineButtons;
-
-    firstLineButtons.push_back(CreateButtonTG("->", "->"));
-    firstLineButtons.push_back(CreateButtonTG("<-", "<-"));
-    
-    keyBoardList->inlineKeyboard.push_back(firstLineButtons);
-
-    bot.getApi().sendMessage(message->chat->id,"Вот список мероприятий: ", false, 0, keyBoardList);
-
-    return 0;
-}
+        return 0;
+    }  
+};
--- ReviewBot/ReviewBot.h
+++ ReviewBot/ReviewBot.h
@@ -1,24 +1,78 @@
 #ifndef __REVIEW_BOT_H__
 #define __REVIEW_BOT_H__
 
+#include <map>
 
 #include <tgbot/tgbot.h>
-#include <iostream>
-#include "../BotTools/BotTools.h"
-#include <vector>
-#include <cmath>
 
-#define COUNT_EVENT_IN_PAGE 10
-#define Success 0
+#include "Event/Event.h"
+#include "States.h"
 
-int InitReviewBot(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
-int DeleteEventState(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
-std::string GetPageMessage(int page, std::vector<std::string> events);
-int ListEventsState(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
-int StartCreatingState(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
-int MenuCreateEventCP(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
-int ChooseTypeEvent(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
-int MenuCreateCP(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message);
-int MenuDeleteCP(TgBot::Bot& bot, std::shared_ptr<TgBot::Message> message, std::vector<std::string> namesCP);
+#define END_EST 2
+#define SEPARATOR_DEFAULT ';'
+#define REVIEW_BOT "Review"
+
+// ----------------------------- FOR ADD REVIEW DEFINES -----------------------------
+#define ADD_REVIEW "AddReview"
+
+#define YES "YesAnswerMore"
+#define NO "NoAnswerMore"
+#define SKIPADD "SkipAdditional"
+// ----------------------------- FOR ADD EVENT DEFINES -----------------------------
+#define ADD_EVENT "AddEvent"
+
+#define READ_REVIEW "ReadReview"
+
+#define SET_TIME_NOW "SetTimeNow"
+#define SET_TIME "SetTime"
+#define SET_NAME "SetName"
+
+#define SET_ACTIVE "SetActive"
+#define MIND_TYPE "Mind" 
+#define RUN_TYPE "Run"
+#define COMBO_TYPE "Combo"
+#define VIEWER_TYPE "Viewer"
+
+#define SET_QUESTION "SetQuestion"
+#define RELAY_TYPE "Relay"
+#define CP_TYPE "Cp"
+#define TASKS_TYPE "Tasks"
+#define NO_TASKS_TYPE "NoTasks"
+
+#define SET_COMMAND "SetCommand"
+#define COMMAND_TYPE "Command"
+#define ALONE_TYPE "Alone"
+#define ALL_TYPE "All"
+
+#define ADD_EVENT_BD "AddEventBD"
+
+namespace review_bot {
+    typedef std::vector<std::string> vector_string;
+    int InitBot(TgBot::Bot& bot, std::int64_t chatId, bool admin);
+    int CreateEvent(TgBot::Bot& bot, std::int64_t chatId, review_bot::Event event);
+
+    int SetNameState(TgBot::Bot &bot, std::int64_t chatId);
+    int SetTimeState(TgBot::Bot& bot, std::int64_t chatId);
+    int SetBodyState(TgBot::Bot& bot, std::int64_t chatId);
+    int SetStructState(TgBot::Bot& bot, std::int64_t chatId);
+    int SetCommandState(TgBot::Bot& bot, std::int64_t chatId);
+
+    int ChooseEvent(TgBot::Bot& bot, std::int64_t chatId, review_bot::vector_string names);
+    int AddReviewAdditional(TgBot::Bot& bot, std::int64_t chatId);
+    int MoreEventQuestions(TgBot::Bot& bot, std::int64_t chatId);
+    
+    review_bot::vector_string ActiveQuestion(BodyType type);
+    review_bot::vector_string StructQuestion(StructType type);
+    review_bot::vector_string CommandQuestion(CommandType type);
+
+    std::string EstString(review_bot::vector_string ests);
+    bool ValidEst(std::string est);
+
+    std::vector<int> SeparateEst(std::string est);
+    
+    std::string StatisticEst(std::vector<int> ests);
+    std::string StatisticMore(std::vector<int> more);
+    std::string SaveReviews(std::string nameEvent, std::string data, review_bot::vector_string reviews);
+};
 
 #endif
